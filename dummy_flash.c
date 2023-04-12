5caff9518118ca25d18cd301192958275fcd479e
Andrew Villeneuve <andrewmv@gmail.com>
2023-04-11 22:46:38 -0700

    Header file, refactoring

--- dummy_flash.c
+++ dummy_flash.c
@@ -6,73 +6,12 @@
  */
 
 #include <stdio.h>
+#include "dummy_flash.h"
 #include "pico/stdlib.h"
 #include "hardware/pio.h"
 #include "hardware/dma.h"
 #include "hardware/gpio.h"
 #include "dummy_flash.pio.h"
-
-// Define GPIOs
-#define CLK 2
-#define DATA 3
-#define TRIG 4
-#define F1 4
-
-// Define timing constants
-#define CLOCK_US 6
-#define MISO_INIT_US 80
-#define MOSI_INIT_US 150
-#define SYNC_US 260
-
-// Define machine states
-#define STATE_IDLE 0
-#define STATE_TX_MISO 1
-
-const int miso_packet_length = 26;
-const uint8_t miso_packet[] = {
-    0xFF, 
-    0xFD, 
-    0xFF, 
-    0xFF, 
-    0xC9, 
-    0xFF, 
-    0xBF, 
-    0xF7, 
-    0x70, 
-    0x06, 
-    0x7F, 
-    0xBF, 
-    0x4C, 
-    0x00, 
-    0xA8, 
-    0x88, 
-    0x88, 
-    0x88, 
-    0x88, 
-    0x88, 
-    0x88, 
-    0x88, 
-    0x88, 
-    0x1B, 
-    0xFF, 
-    0x1A
-};
-
-volatile absolute_time_t risetime = nil_time;
-volatile uint8_t bytecount = 0;
-volatile uint8_t bitcount = 0;
-volatile uint8_t state = STATE_IDLE;
-
-// DMA and PIO variables
-const PIO miso_pio = 0;
-const uint8_t miso_sm = 0;
-uint8_t miso_dma_chan;
-uint8_t miso_offset;
-
-const PIO mosi_pio = 1;
-const uint8_t mosi_sm = 0;
-uint8_t mosi_dma_chan;
-uint8_t mosi_offset;
 
 // Callbacks for edge changes on CLK
 void clock_edge_callback(uint gpio, uint32_t events) {
@@ -84,7 +23,10 @@ void clock_edge_callback(uint gpio, uint32_t events) {
         absolute_time_t falltime = get_absolute_time();
         duration_us = absolute_time_diff_us(risetime, falltime);
 
-        if (duration_us > SYNC_US) {
+        if (duration_us == 0) {
+            // Ignore first falling edge if we powered on while CLK was high
+            return;
+        } else if (duration_us > SYNC_US) {
             state = STATE_IDLE;
             // Not implemented
         } else if (duration_us > MOSI_INIT_US) {
@@ -163,7 +105,7 @@ int main() {
 
     // Setup DMA
     miso_dma_chan = dma_claim_unused_channel(true);
-    miso_dma_setup(miso_pio, miso_sm, dma_chan);
+    miso_dma_setup(miso_pio, miso_sm, miso_dma_chan);
 
     // Start SM - it will start waiting for data in TX FIFO
     pio_sm_set_enabled(miso_pio, miso_sm, true);
